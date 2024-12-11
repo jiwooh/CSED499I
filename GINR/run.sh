112a6ac565144d19642a62e575be140bc3e4d029
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-12-12 01:04:23 +0900

    fix: remove old training test codes

--- GINR/run.sh
+++ GINR/run.sh
@@ -1,10 +1,3 @@
-# test: bunny
-python train_ginr.py --dataset_dir dataset/bunny_v1       --n_fourier 100 --n_nodes_in_sample 5000 --lr 0.0001 --n_layers 6 --skip=True --sine=True --all_sine=True --max_epochs 1000
-python eval_ginr.py lightning_logs/GINR/xgusdx42/checkpoints/best.ckpt --dataset_dir dataset/bunny_v1 --mesh data_generation/bunny/reconstruction/bun_zipper.ply --n_fourier 100 --key bunny
-
-# test: gc pred to orig obs
-python train_ginr.py --dataset_dir dataset/gcdata --n_fourier 14 --n_nodes_in_sample 65160 --lr 0.001 --n_layers 8 --skip=True --max_epochs 1000
-
 # true: gc m4 to m5
 # input: graphcast mesh size 4 predictions on 2022-01-01T00:00:00
 # target: graphcast mesh size 5 predictions on 2022-01-01T00:00:00
